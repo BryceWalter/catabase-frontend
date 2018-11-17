705a182418876e2be6466a2a62ad4b59d379b2e8
unknown <celi.ho@gmail.com>
2018-11-17 18:52:02 -0500

    pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import "./App.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Catspage from "./pages/Catspage";
+import Catprofile from "./pages/Catprofile";
+import Ownerspage from "./pages/Ownerspage";
+// import Ownerprofile from "./pages/Ownerprofile";
+import Fosterspage from "./pages/Fosterspage";
+import Fosterprofile from "./pages/Fosterprofile";
+import Shelterspage from "./pages/Shelterspage";
+// import Shelterdetails from "./pages/Sheltersdetails";
+import Vetspage from "./pages/Vetspage";
+// import Vetdetails from "./pages/Vetdetails";
+
+
+
 
 class App extends Component {
   render() {
@@ -11,6 +24,18 @@ class App extends Component {
         <div className="container">
           <Route exact path="/" component={Login} />
           <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/cats" component={Catspage} />
+          <Route exact path="/cat" component={Catprofile} />
+          <Route exact path="/owners" component={Ownerspage} />
+          {/* <Route exact path="/owner" component={Ownerprofile} /> */}
+          <Route exact path="/fosters" component={Fosterspage} />
+          <Route exact path="/foster" component={Fosterprofile} />
+          <Route exact path="/shelters" component={Shelterspage} />
+          {/* <Route exact path="/shelter" component={Shelterdetails} /> */}
+          <Route exact path="/vets" component={Vetspage} />
+          {/* <Route exact path="/vet" component={Vetdetails} /> */}
+
+
         </div>
       </Router>
     );
