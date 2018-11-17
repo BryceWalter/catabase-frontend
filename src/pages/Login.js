13e0b1eff631a86f09a997e0f454ab655ba071cd
anil093 <anil.kumar093@outlook.com>
2018-11-17 18:56:07 -0500

    files added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import LoginCard from '../components/Card';
-import Grid from '@material-ui/core/Grid';
+// import React, { Component } from 'react';
+// import LoginCard from '../components/Card';
+// import Grid from '@material-ui/core/Grid';
 
-class Login extends Component {
-    render() {
-        return (
-            <div>
-                <Grid 
-                    container 
-                    justify="center"
-                    alignItems="center" 
-                    spacing={8} 
-                >
-                    <Grid item xs={3}></Grid>
-                    <Grid item xs={6}>
-                        <LoginCard></LoginCard>
-                    </Grid>
-                    <Grid item xs={3}></Grid>
-                </Grid>
-            </div>
-        );
-    }
-}
+// class Login extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Grid
+//                     container
+//                     justify="center"
+//                     alignItems="center"
+//                     spacing={8}
+//                 >
+//                     <Grid item xs={3}></Grid>
+//                     <Grid item xs={6}>
+//                         <LoginCard></LoginCard>
+//                     </Grid>
+//                     <Grid item xs={3}></Grid>
+//                 </Grid>
+//             </div>
+//         );
+//     }
+// }
 
-export default Login;
+// export default Login;
