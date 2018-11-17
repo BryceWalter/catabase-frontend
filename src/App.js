fb6a88d57522d5d9a7aa11cf489d664c3911ba56
Bryce Walter <brycewalterparker@gmail.com>
2018-11-17 03:06:25 -0500

    Material install & first page and component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './pages/Login';
 
 class App extends Component {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import LoginCard from '../components/Card';
+import Grid from '@material-ui/core/Grid';
 
 class Login extends Component {
     render() {
         return (
             <div>
-                <h1>aloha</h1>
+                <Grid 
+                    container 
+                    justify="center"
+                    alignItems="center" 
+                    spacing={8} 
+                >
+                    <Grid item xs={3}></Grid>
+                    <Grid item xs={6}>
+                        <LoginCard></LoginCard>
+                    </Grid>
+                    <Grid item xs={3}></Grid>
+                </Grid>
             </div>
         );
     }
