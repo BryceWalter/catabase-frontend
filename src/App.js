eff6dbbac313998d8ed443008ce9c494b1eadaa0
christine-aqui <aqui.christine@gmail.com>
2018-11-17 20:46:05 -0500

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -15,17 +14,12 @@ import Shelterspage from "./pages/Shelterspage";
 import Vetspage from "./pages/Vetspage";
 // import Vetdetails from "./pages/Vetdetails";
 
-import React, { Component } from 'react'
-import './App.scss'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import CatForm from './pages/CatForm'
-import OnwerForm from './pages/OnwerForm.js'
-import Fosterform from './pages/Fosterform.js'
+import CatForm from "./pages/CatForm";
+import OnwerForm from "./pages/OnwerForm.js";
+import Fosterform from "./pages/Fosterform.js";
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <Router>
         <div className="container">
@@ -41,12 +35,12 @@ class App extends Component {
           {/* <Route exact path="/shelter" component={Shelterdetails} /> */}
           <Route exact path="/vets" component={Vetspage} />
           {/* <Route exact path="/vet" component={Vetdetails} /> */}
-          <Route exact path='/CatForm' component={CatForm} />
-          <Route exact path='/OnwerForm' component={OnwerForm} />
-          <Route exact path='/Fosterform' component={Fosterform} />
+          <Route exact path="/CatForm" component={CatForm} />
+          <Route exact path="/OnwerForm" component={OnwerForm} />
+          <Route exact path="/Fosterform" component={Fosterform} />
         </div>
       </Router>
-    )
+    );
   }
 }
 
