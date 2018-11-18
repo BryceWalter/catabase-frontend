43eb55b77b9bae9ed96ca8ae945dfc66c84edde0
Nahrin <nahrinoda@yahoo.com>
2018-11-17 19:22:29 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,80 @@
 // start with rfc then tab
 import React from "react";
+import Typography from '@material-ui/core/Typography';
+
+
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  toolbarMain: {
+    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+  },
+  mainFeaturedPost: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4,
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 6}px`,
+    [theme.breakpoints.up('md')]: {
+      paddingRight: 0,
+    },
+  },
+  mainGrid: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  card: {
+    display: 'flex',
+  },
+  cardDetails: {
+    flex: 1,
+  },
+  cardMedia: {
+    width: 160,
+  },
+  markdown: {
+    padding: `${theme.spacing.unit * 3}px 0`,
+  },
+  sidebarAboutBox: {
+    padding: theme.spacing.unit * 2,
+    backgroundColor: theme.palette.grey[200],
+  },
+  sidebarSection: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing.unit * 8,
+    padding: `${theme.spacing.unit * 6}px 0`,
+  },
+});
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-3 text-center">
+    <React.Fragment>
+   
+   
+    <footer className="footer">
+    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
       Copyright &copy; {new Date().getFullYear()} Toronto Cat Rescue
+      </Typography>
     </footer>
+    </React.Fragment>
   );
 };
+
